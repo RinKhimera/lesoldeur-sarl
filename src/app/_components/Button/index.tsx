2914f06630d9cab08e2ef6ef926dc0440329d06d
Samuel Pokam <119413624+RinKhimera@users.noreply.github.com>
2024-01-13 11:25:21 +0100

    Header updates

--- src/app/_components/Button/index.tsx
+++ src/app/_components/Button/index.tsx
@@ -1,38 +1,40 @@
-'use client'
+"use client"
 
-import React, { ElementType } from 'react'
-import Link from 'next/link'
+import Link from "next/link"
+import React, { ElementType } from "react"
 
-import classes from './index.module.scss'
+import classes from "./index.module.scss"
 
 export type Props = {
   label?: string
-  appearance?: 'default' | 'primary' | 'secondary' | 'none'
-  el?: 'button' | 'link' | 'a'
+  appearance?: "default" | "primary" | "secondary" | "none"
+  el?: "button" | "link" | "a"
   onClick?: () => void
   href?: string
   newTab?: boolean
   className?: string
-  type?: 'submit' | 'button'
+  type?: "submit" | "button"
   disabled?: boolean
   invert?: boolean
+  children?: React.ReactNode
 }
 
 export const Button: React.FC<Props> = ({
-  el: elFromProps = 'link',
+  el: elFromProps = "link",
   label,
   newTab,
   href,
   appearance,
   className: classNameFromProps,
   onClick,
-  type = 'button',
+  type = "button",
   disabled,
   invert,
+  children,
 }) => {
   let el = elFromProps
 
-  const newTabProps = newTab ? { target: '_blank', rel: 'noopener noreferrer' } : {}
+  const newTabProps = newTab ? { target: "_blank", rel: "noopener noreferrer" } : {}
 
   const className = [
     classes.button,
@@ -41,19 +43,20 @@ export const Button: React.FC<Props> = ({
     invert && classes[`${appearance}--invert`],
   ]
     .filter(Boolean)
-    .join(' ')
+    .join(" ")
 
   const content = (
     <div className={classes.content}>
       <span className={classes.label}>{label}</span>
+      {children}
     </div>
   )
 
-  if (onClick || type === 'submit') el = 'button'
+  if (onClick || type === "submit") el = "button"
 
-  if (el === 'link') {
+  if (el === "link") {
     return (
-      <Link href={href || ''} className={className} {...newTabProps} onClick={onClick}>
+      <Link href={href || ""} className={className} {...newTabProps} onClick={onClick}>
         {content}
       </Link>
     )
