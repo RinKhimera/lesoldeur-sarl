c169c46fddb19a62ac1fb181a0fa13c6abbb1e0f
Samuel Pokam <dixiades@gmail.com>
2024-01-13 01:40:56 +0100

    eslint fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import { Metadata } from 'next'
-
-import { AdminBar } from './_components/AdminBar'
-import { Footer } from './_components/Footer'
-import { Header } from './_components/Header'
-import { Providers } from './_providers'
-import { InitTheme } from './_providers/Theme/InitTheme'
-import { mergeOpenGraph } from './_utilities/mergeOpenGraph'
-
-import './_css/app.scss'
+import { Metadata } from "next"
+import React from "react"
+import { AdminBar } from "./_components/AdminBar"
+import { Footer } from "./_components/Footer"
+import { Header } from "./_components/Header"
+import "./_css/app.scss"
+import { Providers } from "./_providers"
+import { InitTheme } from "./_providers/Theme/InitTheme"
+import { mergeOpenGraph } from "./_utilities/mergeOpenGraph"
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -33,10 +31,10 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 }
 
 export const metadata: Metadata = {
-  metadataBase: new URL(process.env.NEXT_PUBLIC_SERVER_URL || 'https://payloadcms.com'),
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SERVER_URL || "https://payloadcms.com"),
   twitter: {
-    card: 'summary_large_image',
-    creator: '@payloadcms',
+    card: "summary_large_image",
+    creator: "@payloadcms",
   },
   openGraph: mergeOpenGraph(),
 }
